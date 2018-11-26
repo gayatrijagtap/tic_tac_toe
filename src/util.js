852fcb276b338acdf2e4b3f80ccfb6415c89d5d8
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-11-26 10:20:45 +0530

    Refactored: provided validated enter name and enter symbol

--- src/util.js
+++ src/util.js
@@ -1,22 +1,30 @@
 const read = require('readline-sync');
 
 getPlayerDetails = function() {
-  let playerDetails = { firstPlayer : {} , secondPlayer : {} };
-  playerDetails.firstPlayer.name = enterName(); 
-  playerDetails.firstPlayer.symbol = enterSymbol();
-  playerDetails.firstPlayer.moves = [];
-  playerDetails.secondPlayer.name = 'playerBot';
-  playerDetails.secondPlayer.symbol = allocateBotSymbol(playerDetails.firstPlayer.symbol);
-  playerDetails.secondPlayer.moves = [];
-  return playerDetails;
+  let gameDetails = { firstPlayer : {} , secondPlayer : {} };
+  gameDetails.firstPlayer.name = enterName(); 
+  gameDetails.firstPlayer.symbol = enterSymbol();
+  gameDetails.firstPlayer.moves = [];
+  gameDetails.secondPlayer.name = 'bot';
+  gameDetails.secondPlayer.symbol = allocateBotSymbol(gameDetails.firstPlayer.symbol);
+  gameDetails.secondPlayer.moves = [];
+  return gameDetails;
 }
 
 const enterName = function() {
-  return read.question('Enter name:');
+  let name = read.question('Enter name : ');
+  while(!name) {
+    name = read.question('Please enter name : ');
+  }
+  return name;
 }
 
 const enterSymbol = function() {
-  return read.question('Choose symbol "O" or "X":');
+  let symbol = read.question('\nchoose symbol "O" or "X" : ');
+  while(!['O','X'].includes(symbol)) {
+    symbol = read.question('\nIncorrect symbol!! please choose symbol "O" or "X" : ');
+  }
+  return symbol;
 }
 
 const allocateBotSymbol = function(playerSymbol) {
