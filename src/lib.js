adfb7ca61ba89a336ccad6c922ec14e8b3919d30
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-12-08 15:42:59 +0530

    Introduced displayBoard in lib

--- src/lib.js
+++ src/lib.js
@@ -1,1 +1,9 @@
 const { makeUserMove,makeBotMove,allocateBotSymbol,getPlayerDetails,getUserMove,isValidMove,playBot,hasWon } = require('./util.js');
+
+const displayBoard = function(boardDetails) {
+  console.log("\n\n "+boardDetails[1]+" | "+boardDetails[2]+" | "+boardDetails[3]);
+  console.log("---+---+---");
+  console.log(" "+boardDetails[4]+" | "+boardDetails[5]+" | "+boardDetails[6]);
+  console.log("---+---+---");
+  console.log(" "+boardDetails[7]+" | "+boardDetails[8]+" | "+boardDetails[9]+"\n\n");
+}
